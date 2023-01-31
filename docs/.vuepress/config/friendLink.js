5bc7c1fbd1b2f6f1ac0c4833ce9d4332bdb1260b
zengsitong <zengsitong@newhope.cn>
2023-01-31 14:05:21 +0800

    update

--- docs/.vuepress/config/friendLink.js
+++ docs/.vuepress/config/friendLink.js
@@ -1,9 +1,20 @@
 module.exports = [
     {
-        title: '祭夜の咖啡馆',
+        title: 'msojocs',
         desc: '祭夜の咖啡馆|我很好奇！',
         logo: "https://www.jysafe.cn/favicon.ico",
         link: 'https://www.jysafe.cn/'
     },
-
+    {
+        title: '汉堡',
+        desc: 'pa_note_web',
+        logo: "https://avatars.githubusercontent.com/u/8100696?v=4",
+        link: 'http://hanbaoaaa.xyz//'
+    },
+    {
+        title: 'zsf',
+        desc: '黑白の世界',
+        logo: "https://avatars.githubusercontent.com/u/44086018?v=4",
+        link: 'https://y1nhui.com/'
+    }
 ]
